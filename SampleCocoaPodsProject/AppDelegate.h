26fc50b7a96500f6b285f19b2ca27750277c629a
Soumya Ranjan Mohanty <soumyaranjanmohanty@soumyas-macbook-pro.local>
2016-02-06 10:45:44 +0530

    Initial Commit

---
+++ SampleCocoaPodsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleCocoaPodsProject
+//
+//  Created by Soumya Ranjan Mohanty on 06/02/16.
+//  Copyright © 2016 Soumya Ranjan Mohanty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleCocoaPodsProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleCocoaPodsProject
+//
+//  Created by Soumya Ranjan Mohanty on 06/02/16.
+//  Copyright © 2016 Soumya Ranjan Mohanty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
